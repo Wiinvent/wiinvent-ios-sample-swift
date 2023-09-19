326285eeb794d86aa3be5ef5ff8267ba4f2c9007
tony4x8x <95602353+tony4x8x@users.noreply.github.com>
2023-09-19 10:22:41 +0700

    sua style nut bo qua

--- wiinvent-ios-sample-swift/src/Tv360SkipAdsButton.swift
+++ wiinvent-ios-sample-swift/src/Tv360SkipAdsButton.swift
@@ -8,6 +8,7 @@
 
 import Foundation
 import WISDK
+import UIKit
 
 public class Tv360SkipAdsButton: WiAdsSkipButton {
     
@@ -17,10 +18,18 @@ public class Tv360SkipAdsButton: WiAdsSkipButton {
     
     override public func normalUi() {
         super.normalUi()
+        setTitleColor(.white, for: .normal)
+        titleLabel?.font = UIFont.boldSystemFont(ofSize: 18)
+        backgroundColor      = UIColor.black.withAlphaComponent(0.4)
+        layer.cornerRadius   = 4
+        layer.borderWidth = 1
+        layer.borderColor = UIColor.white.cgColor
+        contentEdgeInsets = UIEdgeInsets(top: 10.0, left: 20.0, bottom: 10.0, right: 20.0)
     }
     
     override public func skipUi() {
         super.skipUi()
+        setTitle("Bỏ qua quảng cáo", for: .normal)
     }
     
     required public init?(coder aDecoder: NSCoder) {
