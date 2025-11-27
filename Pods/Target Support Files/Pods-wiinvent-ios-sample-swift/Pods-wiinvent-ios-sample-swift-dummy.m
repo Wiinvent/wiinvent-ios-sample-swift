b89479bc8108969090e64d284913ae78bddb3f99
Jason <vinhhuynh115@gmail.com>
2025-11-27 15:52:37 +0700

    removed pods

--- Pods/Target Support Files/Pods-wiinvent-ios-sample-swift/Pods-wiinvent-ios-sample-swift-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_wiinvent_ios_sample_swift : NSObject
-@end
-@implementation PodsDummy_Pods_wiinvent_ios_sample_swift
-@end
--- Pods/Target Support Files/Pods-wiinvent-ios-sample-swiftTests/Pods-wiinvent-ios-sample-swiftTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_wiinvent_ios_sample_swiftTests : NSObject
-@end
-@implementation PodsDummy_Pods_wiinvent_ios_sample_swiftTests
-@end
